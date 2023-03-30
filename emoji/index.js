c4bc3ced5e4151aff50d2ed1c2306fa840078e6d
Carysa Peeters <carysa.peeters@student.ehb.be>
2023-03-30 10:01:45 +0200

    alle emoji's zijn zichtbaar

--- emoji/index.js
+++ emoji/index.js
@@ -3,6 +3,20 @@
 let emojis = [];
 
 // TODO: fetch your data
-// TODO: use then to wait for the result
-// TODO: load the json from the reponse
+fetch("https://emoji-api.com/emojis?access_key=445e15d2e82d0a2afb9a33e3b52e76baf87b2131")
+	// TODO: use then to wait for the result
+	.then(function (response) {
+		return response.json();
+	})
+
+	// TODO: load the json from the reponse
+	.then(function (data) {
+		console.log(data);
+		emojis = data;
+		console.log(emojis);
+
+		emojis.forEach((element) => {
+			document.querySelector("#list").insertAdjacentHTML("beforeend", element.character);
+		});
+	});
 // TODO: in the final then function, process the data and add it to the HTML
