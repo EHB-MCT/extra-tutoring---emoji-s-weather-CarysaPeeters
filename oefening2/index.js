c2fdce27db8328d010959f28ea67fe5bbe12464b
Carysa Peeters <carysa.peeters@student.ehb.be>
2023-04-19 17:38:44 +0200

    bijles gedaan

--- oefening2/index.js
+++ oefening2/index.js
@@ -1,24 +1,40 @@
 "use strict";
 
 const weatherApp = {
-  init: {
+	init() {
+		document.getElementById("form").addEventListener("submit", function (e) {
+			e.preventDefault();
+			console.log("submitted");
+			const cityName = document.getElementById("search").value;
+			weatherApp.fetchWeatherData(cityName);
+		});
+	},
 
-  },
-  fetchWeatherData(city) {
+	fetchWeatherData(city) {
+		fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=d7b955c4c268fe54649d6f0d702b39d1&units=metric`)
+			.then(function (response) {
+				return response.json();
+			})
+			.then(function (data) {
+				console.log(data);
+				weatherApp.renderWeatherData(data);
+			});
+	},
 
-  },
-  renderWeatherData(weatherData) {
+	renderWeatherData(weatherData) {
+		//Template
+		console.log(weatherData);
+		document.getElementById("weatherResult").insertAdjacentHTML(
+			"beforeend",
+			` <h2>Weather</h2>
+    <ul>
+      <li>Average tempature: ${weatherData.main.temp} °C</li>
+      <li>Min tempature: ${weatherData.main.temp_min} °C</li>
+      <li>Max tempature: ${weatherData.main.temp_max} °C</li>
+      <li>Description: ${weatherData.weather.description}</li>
+    </ul>`
+		);
+	},
+};
 
-    //Template
-    /*
-    <h2>Weather</h2>
-        <ul>
-          <li>Average tempature: ${temp} °C</li>
-          <li>Min tempature: ${temp_min} °C</li>
-          <li>Max tempature: ${temp_max} °C</li>
-          <li>Description: ${description}</li>
-        </ul>
-    */
-
-  }
-}
+weatherApp.init();
